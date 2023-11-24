dd715d84ebd9b5074ec4553551aa14e55cef4f90
Александр Белов <seriousguytuwan@gmail.com>
2023-11-24 21:52:17 +0500

    Added PostView

--- src/views/PostView/PostView.vue
+++ src/views/PostView/PostView.vue
@@ -1,13 +1,33 @@
 <template>
     <Layout>
-        
+        <div class="container">
+                <div class="img_container">
+                    <img :src="data.img" class="img" alt="">
+                </div>
+                <div class="content">
+                    <p class="date">{{data.date}}</p>
+                    <h1>{{ data.title }}</h1>
+                    <p>{{ data.desc }}</p>
+                </div>
+        </div>
     </Layout>
 </template>
 
 <script setup lang="ts">
 import Layout from '@/layouts/Layout/PageLayout.vue';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
+
+const route = useRoute()
+const data = ref([])
+const postId = +route.params.id
+
+onMounted(async() => {
+    const res = await axios.get(`https://blog-backend-rosy.vercel.app/api/posts/${postId}`)
+    data.value = res.data
+    console.log(data.value.date)
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped lang="scss" src="./PostView.style.scss"></style>
