a28b761d6fa99a6b0eca8fd61395eb46d5370521
Александр Белов <seriousguytuwan@gmail.com>
2023-11-24 22:36:32 +0500

    Change styles and fix PostView

--- src/views/PostView/PostView.vue
+++ src/views/PostView/PostView.vue
@@ -2,10 +2,19 @@
     <Layout>
         <div class="container" v-if="data">
                 <div class="img_container">
-                    <img :src="data.img" class="img" alt="">
+                    <img :src="data.img" class="img" alt="img">
                 </div>
                 <div class="content">
-                    <p class="date">{{data.date}}</p>
+                    <p class="date">{{data.date.replace(/\T.*/, "")}}</p>
+                    <div class="author">
+                        <div v-if="data.userImg" class="avatar">
+                            <img :src="data.userImg" alt="img">
+                        </div>
+                        <div v-else class="avatar">
+                            <img src="../../assets/images/user.svg" alt="">
+                        </div>
+                        <span>{{data.username}}</span>
+                    </div>
                     <h1>{{ data.title }}</h1>
                     <p>{{ data.desc }}</p>
                 </div>
@@ -23,13 +32,14 @@ import { useRoute } from 'vue-router';
 import axios from 'axios';
 
 const route = useRoute()
-const data = ref([])
+const data = ref()
 const postId = +route.params.id
 
 onMounted(async() => {
     try {
         const res = await axios.get(`https://blog-backend-rosy.vercel.app/api/posts/${postId}`)
         data.value = res.data
+        console.log(res.data)
     } catch (error) {
         console.log(error)
     }
