d72f215e9bb4caed6518f133ac77944b210807b8
Alexandr <seriousguytuwan@gmail.com>
2023-11-30 14:46:53 +0300

    Added search to postsStore, added button back to all

--- src/components/PostsList/PostsList.vue
+++ src/components/PostsList/PostsList.vue
@@ -1,6 +1,12 @@
 <template>
     <h1 class="title" :class="{list: isBlogList}">Other Blog Posts</h1>
     <div class="container" :class="{list: isBlogList}">
+    <button class="back" 
+    :class="{active: btnVisible}" 
+    @click="backToAllPosts"
+    >
+        <img src="../../assets/images/back.svg" alt="back">
+    </button>
         <div v-for="card in data" :key="card.id">
             <PostCard 
             :title="card.title" 
@@ -17,7 +23,17 @@
 
 <script setup lang="ts">
     import PostCard from '@/components/Card/PostCard.vue'
+import { usePostsStore } from '@/store/posts';
+import { storeToRefs } from 'pinia';
     const props = defineProps(["data", "isBlogList"])
+
+    const postsStore = usePostsStore()
+    const { getAllPosts } = postsStore
+    const { btnVisible } = storeToRefs(postsStore)
+
+    const backToAllPosts = () => {
+        getAllPosts()
+    }
 
 </script>
 
